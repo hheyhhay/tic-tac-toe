36a673336f161a8b374545d4d6ec8ce8f874fff3
Hayley Dixon <hdixon37@gmail.com>
2021-06-10 15:04:46 -0600

    Build the Player class

---
+++ player.js
@@ -0,0 +1,13 @@
+class Player{
+  constructor(id, token, wins){
+    this.id = id;
+    this.token = token;
+    this.wins = wins;
+  }
+  saveWinsToStorage(){
+
+  }
+  retrieveWinsFromStorage(){
+
+  }
+};
