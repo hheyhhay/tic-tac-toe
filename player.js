f9749e18ad9262dd6e0a1685313474ec3c69217c
Hayley Dixon <hdixon37@gmail.com>
2021-06-11 13:44:16 -0600

    update constructor)

--- player.js
+++ player.js
@@ -1,13 +1,22 @@
 class Player{
-  constructor(id, token, wins){
-    this.id = id;
-    this.token = token;
-    this.wins = wins;
+  constructor(id, token){
+    this.id = id; // dog or cat
+    this.token = token ;
+    this.wins = 0; //going to update each time the player wins ++
   }
   saveWinsToStorage(){
 
   }
   retrieveWinsFromStorage(){
-
   }
 };
+// constructor - properties should include: id (ex: 'one'), token (ex: '⭐️'), wins (ex: [])
+// saveWinsToStorage
+// retrieveWinsFromStorage
+
+
+// example instantiation:
+// var dogPlayer = new Player('dog', 🐶)
+
+
+// "🐶"|| "🐱"
