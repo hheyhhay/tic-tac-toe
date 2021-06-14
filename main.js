8ed83f0830b60cf4ae84c794084fcaf69a747337
Hayley Dixon <hdixon37@gmail.com>
2021-06-14 16:24:41 -0600

    Change checksWinner method name to checksForWinner

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var currentGame = new Game(dogPlayer, catPlayer);
 // sets up querySelectors
 var gameBoard = document.getElementById('gameboard');
 var middleSection = document.getElementById('middle');
-var headerText = document.getElementById('turn-txt');
+var header = document.getElementById('turn-txt');
 var dogWins = document.getElementById('dog-wins')
 var catWins = document.getElementById('cat-wins')
 
@@ -28,19 +28,10 @@ function getTarget(event){
   if (!event){
     event = window.event;
   }
-  return console.log(event.target || event.srcElement);
+  return event.target;
 }
 
-function gameProcess(){
-  var currentWinner
-  currentGame.checksWinner();
-  if (currentGame.hasWinner === true){
-    currentWinner = currentGame.winner;
-    updateWinner(currentWinner);
-  }
-  currentGame.updatesPlayer()
-  renderBoard()
-}
+
 
 function updateCell(event) {
   selectedCell = event.target.parentNode.parentNode;
@@ -59,43 +50,47 @@ function updateCell(event) {
   }
 };
 
-
-
-
-function renderGameBoard(){
-    var gameBoardHTML = "";
-    gameBoardHTML=  `
-      <div class="cell cell-0" id = "cell-0">${currentGame.placement[0]}</div>
-      <div class="cell cell-1">${currentGame.placement[1]}</div>
-      <div class="cell cell-2">${currentGame.placement[2]}</div>
-      <div class="cell cell-3">${currentGame.placement[3]}</div>
-      <div class="cell cell-4">${currentGame.placement[4]}</div>
-      <div class="cell cell-5">${currentGame.placement[5]}</div>
-      <div class="cell cell-6">${currentGame.placement[6]}</div>
-      <div class="cell cell-7">${currentGame.placement[7]}</div>
-      <div class="cell cell-8">${currentGame.placement[8]}</div>
-    `
-    gameBoard.innerHTML = gameBoardHTML
-}
-
-function renderHeader(){
-  var headerText = "";
-  if(!currentGame.placement.includes("") && !currentGame.hasWinner){ //icons are full & its a draw
-      headerText = `It's a Draw - click any square to reset game`
-  } else if (currentGame.hasWinner === false){
-    headerText = `It's ${currentGame.icon} turn`
-  } else if (currentGame.hasWinner === true){
-      headerText = ` ${currentGame.winner.token} is winner - click any square to restart`
+function gameProcess(){
+  var currentWinner
+  currentGame.checksWinner();
+  if (currentGame.hasWinner === true){
+    currentWinner = currentGame.winner;
+    updateWinner(currentWinner);
   }
-  headerText.innerText = headerText;
+  currentGame.updatesPlayer()
+  renderBoard()
 }
-
 
 function renderBoard(){
   renderHeader();
   renderGameBoard();
   }
 
+function renderGameBoard(){
+    var gameBoardHTML = "";
+    gameBoardHTML=  `<div class="cell cell-0" id = "cell-0">${currentGame.placement[0]}</div>
+        <div class="cell cell-1">${currentGame.placement[1]}</div>
+        <div class="cell cell-2">${currentGame.placement[2]}</div>
+        <div class="cell cell-3">${currentGame.placement[3]}</div>
+        <div class="cell cell-4">${currentGame.placement[4]}</div>
+        <div class="cell cell-5">${currentGame.placement[5]}</div>
+        <div class="cell cell-6">${currentGame.placement[6]}</div>
+        <div class="cell cell-7">${currentGame.placement[7]}</div>
+        <div class="cell cell-8">${currentGame.placement[8]}</div>`
+    gameBoard.innerHTML = gameBoardHTML
+};
+
+function renderHeader(){
+  var headerText = "";
+  if(!currentGame.placement.includes("") && !currentGame.hasWinner){ //icons are full & its a draw
+      headerText = `It's a Draw - click any square to reset game`;
+  } else if (currentGame.hasWinner === false){
+    headerText = `It's ${currentGame.icon} turn`;
+  } else if (currentGame.hasWinner === true){
+      headerText = ` ${currentGame.winner.token} is winner - click any square to restart`;
+  }
+  header.innerText = headerText;
+};
 
 function updateWinner(winningPlayer){
   if (winningPlayer === currentGame.dogPlayer){
