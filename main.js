d7a2ee94f51e4977ebe19563f29b62a155749bd8
Hayley Dixon <hdixon37@gmail.com>
2021-06-13 20:00:57 -0600

    Clean up code & add ID to middle section

--- main.js
+++ main.js
@@ -17,9 +17,10 @@ var index7 = document.getElementById('cell-7');
 var index8 = document.getElementById('cell-8');
 var gameBoard = document.getElementById('gameboard');
 var middleSection = document.getElementById('middle');
+var changeTurnText = document.getElementById('turn-text');
 
 //event listeners
-gameBoard.addEventListener('click', updateCell);
+middleSection.addEventListener('click', updateCell);
 window.addEventListener('load', intializeGame);
 
 
@@ -27,10 +28,6 @@ console.log(newGame.placement)
 console.log(newGame.placement.length)
 console.log(newGame.icon);
 
-
-// console.log('player1', player1);
-// console.log('player1[player1.token]', player1[player1.token]);
-// console.log("player1[token]",player1[token]);
 
 // on page load, the default player is player1.
 function intializeGame(){
@@ -46,59 +43,50 @@ function getTarget(event){
   return console.log(event.target || event.srcElement);
 }
 
+function gameProcess(){
+  newGame.checksWinner();
+  newGame.updatesPlayer()
+  renderBoard()
+}
+
 function updateCell(event) {
-  selectedCell = event.target.parentNode;
+  selectedCell = event.target.parentNode.parentNode;
+  console.log(selectedCell)
   var target;
   target = getTarget(event)
   // if index0 is clicked, the console logs 'hi',
   // updates the DM (this.placement), then it changes who the player is, then it renders/
   //the board w/ the updated information.
+
+  // I think i can refractor this w/ an array?
   if(event.target.className === "cell cell-0"){
     console.log('hi')
     newGame.trackGame('0');
-    newGame.checksWinner();
-    renderBoard();
-    newGame.updatesPlayer()
+    gameProcess()
   } if (event.target.className === 'cell cell-1'){
     newGame.trackGame('1');
-    newGame.checksWinner();
-    renderBoard();
-    newGame.updatesPlayer()
+    gameProcess()
   } if (event.target.className === 'cell cell-2'){
     newGame.trackGame('2');
-    newGame.checksWinner();
-    renderBoard();
-    newGame.updatesPlayer()
+    gameProcess()
   } if (event.target.className === 'cell cell-3'){
     newGame.trackGame('3');
-    newGame.checksWinner();
-    renderBoard();
-    newGame.updatesPlayer()
+    gameProcess()
   }if (event.target.className === 'cell cell-4'){
     newGame.trackGame('4');
-    newGame.checksWinner();
-    renderBoard();
-    newGame.updatesPlayer()
+    gameProcess()
   }if (event.target.className === 'cell cell-5'){
     newGame.trackGame('5');
-    newGame.checksWinner();
-    renderBoard();
-    newGame.updatesPlayer()
+    gameProcess()
   }if (event.target.className === 'cell cell-6'){
     newGame.trackGame('6');
-    newGame.checksWinner();
-    renderBoard();
-    newGame.updatesPlayer()
+    gameProcess()
   } if (event.target.className === 'cell cell-7'){
     newGame.trackGame('7');
-    newGame.checksWinner();
-    renderBoard();
-    newGame.updatesPlayer()
+    gameProcess()
   } if (event.target.className === 'cell cell-8'){
     newGame.trackGame('8');
-    newGame.checksWinner();
-    renderBoard();
-    newGame.updatesPlayer()
+    gameProcess()
   }
 };
 
@@ -111,7 +99,20 @@ function renderBoard(){
   // for (var i = 0; i<newGame.placement.length; i++){
   //
   // }
-    boardHTML = `<div class="cell cell-0">${newGame.placement[0]}</div>
+    //  `<div class="cell cell-0">${newGame.placement[0]}</div>
+    //             <div class="cell cell-1">${newGame.placement[1]}</div>
+    //             <div class="cell cell-2">${newGame.placement[2]}</div>
+    //             <div class="cell cell-3">${newGame.placement[3]}</div>
+    //             <div class="cell cell-4">${newGame.placement[4]}</div>
+    //             <div class="cell cell-5">${newGame.placement[5]}</div>
+    //             <div class="cell cell-6">${newGame.placement[6]}</div>
+    //             <div class="cell cell-7">${newGame.placement[7]}</div>
+    //             <div class="cell cell-8">${newGame.placement[8]}</div>`;
+    // turnText = `It's ${newGame.icon} turn`
+
+    boardHTML =   `<h1 id = "turn-txt">It's ${newGame.icon} turn</h1>
+              <div class = "grid" id="gameboard">
+                <div class="cell cell-0" id = "cell-0">${newGame.placement[0]}</div>
                 <div class="cell cell-1">${newGame.placement[1]}</div>
                 <div class="cell cell-2">${newGame.placement[2]}</div>
                 <div class="cell cell-3">${newGame.placement[3]}</div>
@@ -119,8 +120,8 @@ function renderBoard(){
                 <div class="cell cell-5">${newGame.placement[5]}</div>
                 <div class="cell cell-6">${newGame.placement[6]}</div>
                 <div class="cell cell-7">${newGame.placement[7]}</div>
-                <div class="cell cell-8">${newGame.placement[8]}</div>`;
-
+                <div class="cell cell-8">${newGame.placement[8]}</div>
+              </div>`
 
             // <div class="cell cell-0" id = "cell-0">🐶 </div>
             // <div class="cell cell-1">🐶</div>
@@ -133,12 +134,8 @@ function renderBoard(){
             // <div class="cell cell-8">🐶</div>`
 
 
-
-
-
-
-
-  gameboard.innerHTML = boardHTML;
+  middleSection.innerHTML = boardHTML;
+  // changeTurnText.innerText = turnText;
 }
 
 
