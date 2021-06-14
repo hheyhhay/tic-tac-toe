2fe801e5cf8fd7df2bcdc072ab9435c8a869c66a
Hayley Dixon <hdixon37@gmail.com>
2021-06-14 13:47:59 -0600

    Changes header Render and includes draw factor

--- main.js
+++ main.js
@@ -17,7 +17,9 @@ var index7 = document.getElementById('cell-7');
 var index8 = document.getElementById('cell-8');
 var gameBoard = document.getElementById('gameboard');
 var middleSection = document.getElementById('middle');
-var changeTurnText = document.getElementById('turn-text');
+var changeTurnText = document.getElementById('turn-txt');
+var dogWins = document.getElementById('dog-wins')
+var catWins = document.getElementById('cat-wins')
 
 //event listeners
 middleSection.addEventListener('click', updateCell);
@@ -33,6 +35,7 @@ console.log(newGame.icon);
 function intializeGame(){
   console.log('!')
   newGame.updatesPlayer();
+  renderBoard();
   // newGame.player1.isTurn = true;
 }
 
@@ -44,16 +47,23 @@ function getTarget(event){
 }
 
 function gameProcess(){
+  var currentWinner
   newGame.checksWinner();
+  if (newGame.hasWinner === true){
+    currentWinner = newGame.winner;
+    updateWinner(currentWinner);
+  }
   newGame.updatesPlayer()
   renderBoard()
 }
 
 function updateCell(event) {
   selectedCell = event.target.parentNode.parentNode;
-  console.log(selectedCell)
   var target;
   target = getTarget(event)
+  if (newGame.hasWinner){
+    clearBoard()
+  }
   // if index0 is clicked, the console logs 'hi',
   // updates the DM (this.placement), then it changes who the player is, then it renders/
   //the board w/ the updated information.
@@ -93,51 +103,93 @@ function updateCell(event) {
 
 
 
-
-function renderBoard(){
-  var boardHTML = ""
-  // for (var i = 0; i<newGame.placement.length; i++){
-  //
-  // }
-    //  `<div class="cell cell-0">${newGame.placement[0]}</div>
-    //             <div class="cell cell-1">${newGame.placement[1]}</div>
-    //             <div class="cell cell-2">${newGame.placement[2]}</div>
-    //             <div class="cell cell-3">${newGame.placement[3]}</div>
-    //             <div class="cell cell-4">${newGame.placement[4]}</div>
-    //             <div class="cell cell-5">${newGame.placement[5]}</div>
-    //             <div class="cell cell-6">${newGame.placement[6]}</div>
-    //             <div class="cell cell-7">${newGame.placement[7]}</div>
-    //             <div class="cell cell-8">${newGame.placement[8]}</div>`;
-    // turnText = `It's ${newGame.icon} turn`
-
-    boardHTML =   `<h1 id = "turn-txt">It's ${newGame.icon} turn</h1>
-              <div class = "grid" id="gameboard">
-                <div class="cell cell-0" id = "cell-0">${newGame.placement[0]}</div>
-                <div class="cell cell-1">${newGame.placement[1]}</div>
-                <div class="cell cell-2">${newGame.placement[2]}</div>
-                <div class="cell cell-3">${newGame.placement[3]}</div>
-                <div class="cell cell-4">${newGame.placement[4]}</div>
-                <div class="cell cell-5">${newGame.placement[5]}</div>
-                <div class="cell cell-6">${newGame.placement[6]}</div>
-                <div class="cell cell-7">${newGame.placement[7]}</div>
-                <div class="cell cell-8">${newGame.placement[8]}</div>
-              </div>`
-
-            // <div class="cell cell-0" id = "cell-0">🐶 </div>
-            // <div class="cell cell-1">🐶</div>
-            // <div class="cell cell-2">🐶</div>
-            // <div class="cell cell-3">🐶</div>
-            // <div class="cell cell-4">🐶</div>
-            // <div class="cell cell-5">🐶</div>
-            // <div class="cell cell-6">🐶</div>
-            // <div class="cell cell-7">🐶</div>
-            // <div class="cell cell-8">🐶</div>`
-
-
-  middleSection.innerHTML = boardHTML;
-  // changeTurnText.innerText = turnText;
+function renderGameBoard(){
+    var gameBoardHTML = "";
+    gameBoardHTML=  `
+      <div class="cell cell-0" id = "cell-0">${newGame.placement[0]}</div>
+      <div class="cell cell-1">${newGame.placement[1]}</div>
+      <div class="cell cell-2">${newGame.placement[2]}</div>
+      <div class="cell cell-3">${newGame.placement[3]}</div>
+      <div class="cell cell-4">${newGame.placement[4]}</div>
+      <div class="cell cell-5">${newGame.placement[5]}</div>
+      <div class="cell cell-6">${newGame.placement[6]}</div>
+      <div class="cell cell-7">${newGame.placement[7]}</div>
+      <div class="cell cell-8">${newGame.placement[8]}</div>
+    `
+    gameBoard.innerHTML = gameBoardHTML
 }
 
+function renderHeader(){
+  var headerText = "";
+  if(!newGame.placement.includes("") && !newGame.hasWinner){ //icons are full & its a draw
+      headerText = `It's a Draw - click any square to reset game`
+  } else if (newGame.hasWinner === false){
+    headerText = `It's ${newGame.icon} turn`
+  } else if (newGame.hasWinner === true){
+      headerText = ` ${newGame.winner.token} is winner :)`
+  }
+  changeTurnText.innerText = headerText;
+}
+
+
+function renderBoard(){
+  renderHeader();
+  renderGameBoard();
+
+
+  //     var boardHTML = ""
+  // if (newGame.hasWinner === false){
+  //
+  //     boardHTML =   `<h1 id = "turn-txt">${newGame.winner.token} has WON! CLICK to restart Game</h1>
+  //               <div class = "grid" id="gameboard">
+  //                 <div class="cell cell-0" id = "cell-0">${newGame.placement[0]}</div>
+  //                 <div class="cell cell-1">${newGame.placement[1]}</div>
+  //                 <div class="cell cell-2">${newGame.placement[2]}</div>
+  //                 <div class="cell cell-3">${newGame.placement[3]}</div>
+  //                 <div class="cell cell-4">${newGame.placement[4]}</div>
+  //                 <div class="cell cell-5">${newGame.placement[5]}</div>
+  //                 <div class="cell cell-6">${newGame.placement[6]}</div>
+  //                 <div class="cell cell-7">${newGame.placement[7]}</div>
+  //                 <div class="cell cell-8">${newGame.placement[8]}</div>
+  //               </div>`
+  //               middleSection.innerHTML = boardHTML;
+  // } if (newGame.hasWinner === true){
+  //   boardHTML =   `<h1 id = "turn-txt"> ${newGame.winner.token} has WON! CLICK to restart Game </h1>
+  //             <div class = "grid" id="gameboard">
+  //               <div class="cell cell-0" id = "cell-0">${newGame.placement[0]}</div>
+  //               <div class="cell cell-1">${newGame.placement[1]}</div>
+  //               <div class="cell cell-2">${newGame.placement[2]}</div>
+  //               <div class="cell cell-3">${newGame.placement[3]}</div>
+  //               <div class="cell cell-4">${newGame.placement[4]}</div>
+  //               <div class="cell cell-5">${newGame.placement[5]}</div>
+  //               <div class="cell cell-6">${newGame.placement[6]}</div>
+  //               <div class="cell cell-7">${newGame.placement[7]}</div>
+  //               <div class="cell cell-8">${newGame.placement[8]}</div>
+  //             </div>`
+  //             middleSection.innerHTML = boardHTML;
+  // } if
+
+  }
+
+
+function updateWinner(winningPlayer){
+  if (winningPlayer === newGame.player1){
+    dogWins.innerText = `${newGame.player1.wins} wins!`
+  } else if (winningPlayer === newGame.player2){
+    catWins.innerText =`${newGame.player2.wins} wins`
+    }
+  }
+  function clearBoard(){
+    newGame.placement = ["","","", "","","","","","",];
+    newGame.hasWinner = false;
+    renderBoard();
+  }
+
+function checksForDraw(){
+  if(!newGame.placement.includes("") && !newGame.hasWinner){ //icons are full & its a draw
+
+  }
+}
 
 
 
